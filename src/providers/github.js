66c36387fc3dcb3f546166f5fa58964934f3ab90
Bill Trikalinos <billtrik@gmail.com>
2017-03-09 00:41:17 +0100

    Create GithubAPI provider with some simple getters

---
+++ src/providers/github.js
@@ -0,0 +1,33 @@
+import fetcher from '../utilities/fetcher';
+
+const baseUrl = 'https://api.github.com';
+
+function getFetchOptions(extraOptions) {
+  return {
+    method: 'GET',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      'Content-Type': 'application/json',
+    },
+    ...extraOptions,
+  };
+}
+
+const fetchAllUsers = () => {
+  const url = `${baseUrl}/users`;
+  const options = getFetchOptions();
+
+  return fetcher(url, options);
+};
+
+const fetchUser = (username) => {
+  const url = `${baseUrl}/users/${username}`;
+  const options = getFetchOptions();
+
+  return fetcher(url, options);
+};
+
+export default {
+  fetchAllUsers,
+  fetchUser,
+};
